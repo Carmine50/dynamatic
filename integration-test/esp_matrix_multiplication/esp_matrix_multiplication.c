bd0110f2ef18423fa1ade85aa20be2c91936062c
Carmine Rizzi <crizzi@ethz.ch>
2024-09-03 16:50:14 +0200

    Added esp matrix multiplication benchmark

---
+++ integration-test/esp_matrix_multiplication/esp_matrix_multiplication.c
@@ -0,0 +1,43 @@
+#include "esp_matrix_multiplication.h"
+#include "dynamatic/Integration.h"
+
+
+//int esp_matrix_multiplication(in_int_t conf_info_nbursts, in_int_t conf_info_mat_1_size, in_int_t conf_info_mat_out_size , in_int_t conf_info_fpsa, 
+//                              in_int_t conf_info_op_mode, in_long_t fifo_in, out_long_t fifo_out, out_int_t load_ctrl, out_int_t store_ctrl){
+int esp_matrix_multiplication(in_int_t conf_info_nbursts, in_int_t conf_info_mat_1_size, in_int_t conf_info_mat_out_size , in_int_t conf_info_fpsa, in_int_t conf_info_op_mode, in_long_t fifo_in){
+
+
+  int iBurst, iPacket;
+  long result_fifo_out;
+  for (iBurst = 0; iBurst < conf_info_nbursts; iBurst++)
+  {
+    external_function( __esp_LoadReqESP(iBurst, conf_info_mat_1_size, conf_info_mat_out_size) );
+    //long value = __fifo(fifo_in);
+    //value =
+    for ( iPacket = 0; iPacket < conf_info_mat_1_size ; iPacket++ ){
+      result_fifo_out =  __esp_stream(fifo_in, conf_info_op_mode, conf_info_fpsa);
+      return external_function( result_fifo_out );
+    }
+
+
+    external_function( __esp_StoreReqESP(iBurst, conf_info_mat_1_size, conf_info_mat_out_size) );
+  }
+
+  //long result_fifo_out =  __esp_stream(fifo_in, conf_info_op_mode, conf_info_fpsa);
+  //return external_function( result_fifo_out );
+  return 1;
+}
+
+int main(void) {
+  int a = 10;
+  int b = 8;
+  int c = 13;
+  int d = 13;
+  int e = 13;
+  long f = 13;
+
+  CALL_KERNEL(esp_matrix_multiplication, a, b, c, d, e, f);
+  return 0;
+}
+
+
---
+++ integration-test/esp_matrix_multiplication/esp_matrix_multiplication.h
@@ -0,0 +1,29 @@
+#ifndef EXTERNAL_EXTERNAL_H
+#define EXTERNAL_EXTERNAL_H
+
+typedef int out_int_t; 
+typedef int in_int_t;
+
+typedef long out_long_t; 
+typedef long in_long_t;
+
+long __esp_stream(long fifo_in, int conf_info_op_mode, int conf_info_fpsa);
+long __esp_LoadReqESP(int iBurst, int conf_info_mat_1_size, int conf_info_mat_out_size);
+long __esp_StoreReqESP(int iBurst, int conf_info_mat_1_size, int conf_info_mat_out_size);
+long external_function(long out);
+
+long __fifo(long ins);
+long __decoder(long data_in);
+long __systolic_unit(int start, int tile_op_mode, int tile_fpsa_config, long data_in_1, long data_in_2);
+long __systolic_ctrl_unit(long data_in, int condition);
+
+
+//int esp_matrix_multiplication(in_int_t conf_info_nbursts, in_int_t conf_info_mat_1_size, in_int_t conf_info_mat_out_size , in_int_t conf_info_fpsa, 
+//                              in_int_t conf_info_op_mode, in_long_t fifo_in, out_long_t fifo_out, out_int_t load_ctrl, out_int_t store_ctrl);
+int esp_matrix_multiplication(in_int_t conf_info_nbursts, in_int_t conf_info_mat_1_size, in_int_t conf_info_mat_out_size , in_int_t conf_info_fpsa, in_int_t conf_info_op_mode, in_long_t fifo_in);
+
+
+
+#endif // EXTERNAL_EXTERNAL_H
+
+
