6d03d22637d53ae4c146518ddba61b045ee102b7
Carmine Rizzi <crizzi@ethz.ch>
2024-09-08 18:28:17 +0200

    Updated the configurations for load and store esp operations

--- data/verilog/support/LoadReqESP.v
+++ data/verilog/support/LoadReqESP.v
@@ -1,22 +1,33 @@
-module LoadReqESP #(parameter INPUTS = 1,
-                parameter OUTPUTS = 1,
-                parameter DATA_IN_SIZE = 32,
-                parameter DATA_OUT_SIZE = 32)
-        (
-                input clk,
-                input rst,
-                input [INPUTS * (DATA_IN_SIZE)- 1 : 0]data_in_bus,
-                input [INPUTS - 1 : 0]valid_in_bus,
-                output [INPUTS - 1 : 0] ready_in_bus,
+module LoadReqESP #(
+  parameter DATA_TYPE = 32
+)(
+    input clk,
+    input rst,
+    input [ DATA_TYPE - 1 : 0] conf_info_mat_1_size,
+    input conf_info_mat_1_size_valid,
+    output conf_info_mat_1_size_ready,
 
-                output [OUTPUTS * (DATA_OUT_SIZE) - 1 : 0]data_out_bus,
-                output [OUTPUTS - 1 : 0]valid_out_bus,
-                input   [OUTPUTS - 1 : 0] ready_out_bus
+    input [ DATA_TYPE - 1 : 0 ] iBurst,
+    input iBurst_valid,
+    output iBurst_ready,
+
+    output start_ready,
+    input start_valid,
+
+    output end_valid,
+    input end_ready,
+
+    output [ DATA_TYPE - 1 : 0] out0,
+    output  out0_valid,
+    input    out0_ready
 );
 
-    muli #(.DATA_TYPE(DATA_IN_SIZE), .LATENCY(4)) muli0
-    (.clk(clk), .rst(rst), .data_in_bus(data_in_bus), .valid_in_bus(valid_in_bus), .ready_in_bus(ready_in_bus),
-    .data_out_bus(data_out_bus), .valid_out_bus(valid_out_bus), .ready_out_bus(ready_out_bus));
+
+    muli #(.DATA_TYPE(DATA_TYPE), .LATENCY(4)) muli0
+    (.clk(clk), .rst(rst), .lhs(conf_info_mat_1_size), .lhs_valid(conf_info_mat_1_size_valid), .lhs_ready(conf_info_mat_1_size_ready),
+    .rhs(iBurst), .rhs_valid(iBurst_valid), .rhs_ready(iBurst_ready), .result(out0), .result_valid(out0_valid), .result_ready(out0_ready));
+
+   assign start_ready = 1;
 
 endmodule
 
--- data/verilog/support/StoreReqESP.v
+++ data/verilog/support/StoreReqESP.v
@@ -1,48 +1,66 @@
-module StoreReqESP #(parameter INPUTS = 4,
-		parameter OUTPUTS = 1,
-		parameter DATA_IN_SIZE = 32,
-		parameter DATA_OUT_SIZE = 32)
-	(
-		input clk,
-		input rst,
-		input [INPUTS * (DATA_IN_SIZE)- 1 : 0]data_in_bus,
-		input [INPUTS - 1 : 0]valid_in_bus,
-		output [INPUTS - 1 : 0] ready_in_bus,
-		
-		output [OUTPUTS * (DATA_OUT_SIZE) - 1 : 0]data_out_bus,
-		output [OUTPUTS - 1 : 0]valid_out_bus,
-		input 	[OUTPUTS - 1 : 0] ready_out_bus
+module StoreReqESP #(
+    parameter DATA_TYPE = 32
+)(
+    input clk,
+    input rst,
+
+    input [ DATA_TYPE - 1 : 0]conf_info_mat_1_size,
+    input conf_info_mat_1_size_valid,
+    output conf_info_mat_1_size_ready,
+
+    input [ DATA_TYPE - 1 : 0] iBurst,
+    input iBurst_valid,
+    output iBurst_ready,
+
+    input [ DATA_TYPE - 1 : 0]conf_info_mat_out_size,
+    input conf_info_mat_out_size_valid,
+    output conf_info_mat_out_size_ready,
+
+
+    output start_ready,
+    input start_valid,
+
+    output end_valid,
+    input end_ready,
+
+    output [ DATA_TYPE - 1 : 0]out0,
+    output out0_valid,
+    input 	 out0_ready
 );
 
-    wire [(DATA_IN_SIZE)- 1 : 0] result_mul0;
+    wire [ DATA_TYPE - 1 : 0] result_mul0;
     wire valid_mul0;
     wire ready_mul0;
-    wire [(DATA_IN_SIZE)- 1 : 0] result_mul1;
+    wire [ DATA_TYPE - 1 : 0] result_mul1;
     wire valid_mul1;
     wire ready_mul1;
-	wire [3 : 0] readyOutMul;
-	wire joinValid;
-	wire joinReady;	
 
 
-    muli #( .DATA_TYPE(DATA_IN_SIZE), .LATENCY(4)  ) muli0
-    (.clk(clk), .rst(rst), .data_in_bus({data_in_bus[0 * DATA_IN_SIZE +: DATA_IN_SIZE], data_in_bus[2 * DATA_IN_SIZE +: DATA_IN_SIZE]}), .valid_in_bus({valid_in_bus[0], valid_in_bus[2]}), .ready_in_bus({readyOutMul[0], readyOutMul[2]}),
-    .data_out_bus(result_mul0), .valid_out_bus(valid_mul0), .ready_out_bus(ready_mul0));
+    wire [3 : 0] readyOutMul;
+    wire joinValid;
+    wire joinReady;	
 
-    muli #( .DATA_TYPE(DATA_IN_SIZE), .LATENCY(4)  ) muli1
-    (.clk(clk), .rst(rst), .data_in_bus({data_in_bus[1 * DATA_IN_SIZE +: DATA_IN_SIZE], data_in_bus[3 * DATA_IN_SIZE +: DATA_IN_SIZE]}), .valid_in_bus({valid_in_bus[1], valid_in_bus[3]}), .ready_in_bus({readyOutMul[1], readyOutMul[3]}),
-    .data_out_bus(result_mul1), .valid_out_bus(valid_mul1), .ready_out_bus(ready_mul1));
+    assign start_ready = 1;
 
-    addi #( .DATA_TYPE(DATA_IN_SIZE)  ) add0
-    (.clk(clk), .rst(rst), .data_in_bus({result_mul0, result_mul1}), .valid_in_bus({valid_mul0, valid_mul1}), .ready_in_bus({ready_mul0, ready_mul1}),
-    .data_out_bus(data_out_bus), .valid_out_bus(valid_out_bus), .ready_out_bus(ready_out_bus));
+    muli #(.DATA_TYPE(DATA_TYPE), .LATENCY(4)) muli0
+    (.clk(clk), .rst(rst), .lhs(conf_info_mat_1_size), .lhs_valid(conf_info_mat_1_size_valid), .lhs_ready(readyOutMul[0]),
+    .rhs(iBurst), .rhs_valid(iBurst_valid), .rhs_ready(readyOutMul[2]), .result(result_mul0), .result_valid(valid_mul0), .result_ready(ready_mul0));
 
-	// synchronize inputs
-	//
-	 	join_type #(.SIZE(4)) j(.valid_in(valid_in_bus), .ready_in(ready_in_bus), .valid_out(joinValid), .ready_out(joinReady));
-	//
-	 		assign joinReady = readyOutMul[0] & readyOutMul[1] & readyOutMul[2] & readyOutMul[3];
-	//
-	 		endmodule
+    muli #(.DATA_TYPE(DATA_TYPE), .LATENCY(4)) muli1
+    (.clk(clk), .rst(rst), .lhs(conf_info_mat_out_size), .lhs_valid(conf_info_mat_out_size_valid), .lhs_ready(readyOutMul[1]),
+    .rhs(iBurst), .rhs_valid(iBurst_valid), .rhs_ready(readyOutMul[3]), .result(result_mul1), .result_valid(valid_mul1), .result_ready(ready_mul1));
+
+    addi #( .DATA_TYPE(32)  ) add0
+    (.clk(clk), .rst(rst), .lhs(result_mul0), .lhs_valid(valid_mul0), .lhs_ready(ready_mul0), .rhs(result_mul1), .rhs_valid(valid_mul1), .rhs_ready(ready_mul1),
+    .result(out0), .result_valid(out0_valid), .result_ready(out0_ready));
+
+    // synchronize inputs
+    //
+    join_type #(.SIZE(3)) j(.ins_valid({conf_info_mat_1_size_valid, conf_info_fpsa_valid, iBurst_valid}), .ins_ready({conf_info_mat_1_size_ready, conf_info_fpsa_ready, iBurst_ready}), 
+    .outs_valid(joinValid), .outs_ready(joinReady));
+
+      assign joinReady = readyOutMul[0] & readyOutMul[1] & readyOutMul[2] & readyOutMul[3];
+
+endmodule
 	 		
 
