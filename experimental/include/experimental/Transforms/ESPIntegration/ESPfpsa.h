492546488afdd4ec9dbf3fab1d4e41f8ebd04bf3
Carmine50 <crizzi@ethz.ch>
2024-09-09 18:45:00 +0200

    Added new include for new esp integration pass

---
+++ experimental/include/experimental/Transforms/ESPIntegration/ESPfpsa.h
@@ -0,0 +1,34 @@
+//===- ESPfpsa.h - Integration of ESP FPSA in dynamatic -----*- C++ -*-===//
+//
+// Dynamatic is under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the ESP FPSA pass
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef EXPERIMENTAL_TRANSFORMS_ESP_FPSA_H
+#define EXPERIMENTAL_TRANSFORMS_ESP_FPSA_H
+
+#include "dynamatic/Support/DynamaticPass.h"
+#include "dynamatic/Support/LLVM.h"
+
+namespace dynamatic {
+namespace experimental {
+namespace espIntegration {
+
+#define GEN_PASS_DECL_ESPFPSA
+#define GEN_PASS_DEF_ESPFPSA
+#include "experimental/Transforms/Passes.h.inc"
+
+std::unique_ptr<dynamatic::DynamaticPass>
+insertESPfpsa(StringRef fpsaMode = "matMul", StringRef arcPath = "");
+
+} // namespace espIntegration
+} // namespace experimental
+} // namespace dynamatic
+
+#endif // EXPERIMENTAL_TRANSFORMS_ESP_FPSA_H
