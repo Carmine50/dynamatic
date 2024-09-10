c94ad20613181575674864ccdd7e8ee1a1b764f7
Carmine Rizzi <crizzi@ethz.ch>
2024-09-10 23:43:40 +0200

    Updated decoder units

---
+++ data/verilog/support/decoder.v
@@ -0,0 +1,49 @@
+module decoder #(
+  parameter SIZE = 2,
+  parameter DATA_TYPE = 32,
+  parameter SELECT_TYPE = 2
+)(
+		input clk,
+		input rst,
+    // data input channels
+		input [DATA_TYPE - 1 : 0] ins,
+		input ins_valid,
+		output ins_ready,
+    // index input channel
+    input [SELECT_TYPE - 1 : 0] index,
+		input index_valid,
+    output index_ready,
+    // output channel
+		output reg [SIZE * (DATA_TYPE) - 1 : 0] outs,
+		output reg [SIZE - 1 : 0] outs_valid,
+		input 	[SIZE - 1 : 0] outs_ready
+);
+
+	
+  wire joinValid;
+  reg joinReady;
+  integer i;
+
+  always@(posedge clk) begin
+    for (i = 0; i < SIZE ; i=i+1 ) begin
+      if ((i[SELECT_TYPE - 1 : 0] == index) && index_valid == 1 && ins_valid == 1 && ready_out_bus[i] == 1) begin
+        outs_valid[i] <= 1'd1;
+        outs[(i * DATA_TYPE) +: DATA_TYPE ] <= ins[(DATA_TYPE) - 1 : 0] ;
+      end else begin
+        outs_valid[i] <= 1'd0;
+      end
+    end
+  end
+
+  join_type #(
+    .SIZE(SIZE)
+  ) join_inputs (
+    .ins_valid  ({ins_valid, index_valid}),
+    .outs_ready (joinReady             ),
+    .ins_ready  ({ins_ready, index_ready}),
+    .outs_valid (joinValid             )
+  );
+
+	assign joinReady = &outs_ready;
+
+endmodule
---
+++ data/verilog/support/decoderAlt.v
@@ -0,0 +1,52 @@
+module decoderAlt #(
+  parameter SIZE = 2,
+  parameter DATA_TYPE = 32
+)(
+	input clk,
+  input rst,
+  // data input channels
+  input [DATA_TYPE - 1 : 0] ins,
+  input ins_valid,
+  output ins_ready,
+  // output channel
+  output reg [SIZE * (DATA_TYPE) - 1 : 0] outs,
+  output reg [SIZE - 1 : 0] outs_valid,
+  input 	[SIZE - 1 : 0] outs_ready
+);
+
+	
+	reg [$clog2(SIZE) - 1 : 0] condition_next;
+  reg [$clog2(SIZE) - 1 : 0] condition;
+	reg condition_valid;
+	wire condition_ready;
+	
+  decoder #(.SIZE(SIZE), .DATA_TYPE(DATA_TYPE), .SELECT_TYPE($clog2(SIZE))) decoder_unit
+    (.clk(clk), .rst(rst), .ins(ins), .ins_valid(ins_valid), .ins_ready(ins_ready),
+    .index({condition}), .index_valid(condition_valid), .index_ready(condition_ready),
+    .outs(outs), .outs_valid(outs_valid), .outs_ready(outs_ready));
+
+	always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      condition_valid <= 1'b0;
+      condition_next <= 0;
+      condition <= -1;
+    end else if (ins_valid == 1'b1) begin
+      if (outs_ready[condition_next] == 1'b1) begin
+        condition_valid <= 1'b1;
+        if (condition == SIZE - 1) begin
+          condition <= 0;
+        end else begin
+          condition <= condition + 1;
+        end
+        if (condition_next == SIZE - 1) begin
+          condition_next <= 0;
+        end else begin
+          condition_next <= condition_next + 1;
+        end
+      end else begin
+        condition_valid <= 1'b0;
+      end
+    end
+  end
+
+endmodule
