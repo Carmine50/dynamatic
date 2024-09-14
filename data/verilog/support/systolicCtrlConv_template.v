38ed6c563a6d929f3701618af5036d894816036a
Carmine Rizzi <crizzi@ethz.ch>
2024-09-14 13:31:09 +0200

    Added tehb in the sys ctrl conv and added place holders to save matrices with size that is not multiple of 4

--- data/verilog/support/systolicCtrlConv_template.v
+++ data/verilog/support/systolicCtrlConv_template.v
@@ -41,6 +41,7 @@ module systolicCtrlConv #(
   wire condition_valid;
   wire [DATA_TYPE - 1 : 0] dataInSignal;
   wire dataInSignal_valid;
+  wire tehbReady;
 
   assign condition = ins[0 * SELECT_TYPE +: SELECT_TYPE];
   assign condition_valid = ins_valid[0];
@@ -48,9 +49,9 @@ module systolicCtrlConv #(
   assign dataInSignal_valid = ins_valid[1];
 
   assign outs[1 * DATA_TYPE +: SELECT_TYPE] = startSignal;
-  assign outs[0 * DATA_TYPE +: DATA_TYPE] = dataOutSignal;
+  //assign outs[0 * DATA_TYPE +: DATA_TYPE] = dataOutSignal;
   assign outs_valid[1] = startSignal_valid;
-  assign outs_valid[0] = dataOutSignal_valid;
+  //assign outs_valid[0] = dataOutSignal_valid;
   assign ins_ready = readyOut;
 
   always @(posedge clk) begin
@@ -79,9 +80,13 @@ module systolicCtrlConv #(
           if (dataInSignal_valid && start_write == 1'b0) begin
             if (read_inMat == 1'd1) begin
               inputMatrix[cnt_index_inMat] <= dataInSignal[(16 - 1) : 0];
+              // PLACE_HOLDER_INPUT_MATRIX_1
               inputMatrix[cnt_index_inMat+1] <= dataInSignal[(32 - 1) : 16];
+              // PLACE_HOLDER_INPUT_MATRIX_2
               inputMatrix[cnt_index_inMat+2] <= dataInSignal[(48 - 1) : 32];
+              // PLACE_HOLDER_INPUT_MATRIX_3
               inputMatrix[cnt_index_inMat+3] <= dataInSignal[(64 - 1) : 48];
+              // PLACE_HOLDER_INPUT_MATRIX_4
               cnt_index_inMat <= cnt_index_inMat + 4;
               if (cnt_index_kernelMat >= SIZE_KERNEL_MAT ) begin
                 read_inMat <= 1;
@@ -90,9 +95,13 @@ module systolicCtrlConv #(
               end
             end else begin
               kernelMatrix[cnt_index_kernelMat] <= dataInSignal[(16 - 1) : 0];
+              // PLACE_HOLDER_KERNEL_MATRIX_1
               kernelMatrix[cnt_index_kernelMat+1] <= dataInSignal[(32 - 1) : 16];
+              // PLACE_HOLDER_KERNEL_MATRIX_2
               kernelMatrix[cnt_index_kernelMat+2] <= dataInSignal[(48 - 1) : 32];
+              // PLACE_HOLDER_KERNEL_MATRIX_3
               kernelMatrix[cnt_index_kernelMat+3] <= dataInSignal[(64 - 1) : 48];
+              // PLACE_HOLDER_KERNEL_MATRIX_4
               cnt_index_kernelMat <= cnt_index_kernelMat + 4;
               if (cnt_index_inMat >=  SIZE_INPUT_MAT ) begin
                 read_inMat <= 0;
@@ -111,4 +120,17 @@ module systolicCtrlConv #(
 
   RESHAPE_LOOP
   
+  tehb #(
+    .DATA_TYPE(DATA_TYPE)
+  ) data_tehb (
+    .clk        (clk            ),
+    .rst        (rst            ),
+    .ins        (dataOutSignal         ),
+    .ins_valid  (dataOutSignal_valid   ),
+    .ins_ready  (tehbReady   ),
+    .outs       (outs[0 * DATA_TYPE +: DATA_TYPE]        ),
+    .outs_valid (outs_valid[0]  ),
+    .outs_ready (outs_ready[0]  )
+  );
+
 endmodule
