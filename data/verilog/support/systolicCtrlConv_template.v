581e9c20b2189343b023504a2e8fec436f71cb2f
Carmine Rizzi <crizzi@ethz.ch>
2024-09-14 13:33:16 +0200

    Removed placeholders since the change is not needed to adapt to sizes that are not multiple of 4

--- data/verilog/support/systolicCtrlConv_template.v
+++ data/verilog/support/systolicCtrlConv_template.v
@@ -80,13 +80,9 @@ module systolicCtrlConv #(
           if (dataInSignal_valid && start_write == 1'b0) begin
             if (read_inMat == 1'd1) begin
               inputMatrix[cnt_index_inMat] <= dataInSignal[(16 - 1) : 0];
-              // PLACE_HOLDER_INPUT_MATRIX_1
               inputMatrix[cnt_index_inMat+1] <= dataInSignal[(32 - 1) : 16];
-              // PLACE_HOLDER_INPUT_MATRIX_2
               inputMatrix[cnt_index_inMat+2] <= dataInSignal[(48 - 1) : 32];
-              // PLACE_HOLDER_INPUT_MATRIX_3
               inputMatrix[cnt_index_inMat+3] <= dataInSignal[(64 - 1) : 48];
-              // PLACE_HOLDER_INPUT_MATRIX_4
               cnt_index_inMat <= cnt_index_inMat + 4;
               if (cnt_index_kernelMat >= SIZE_KERNEL_MAT ) begin
                 read_inMat <= 1;
@@ -95,13 +91,9 @@ module systolicCtrlConv #(
               end
             end else begin
               kernelMatrix[cnt_index_kernelMat] <= dataInSignal[(16 - 1) : 0];
-              // PLACE_HOLDER_KERNEL_MATRIX_1
               kernelMatrix[cnt_index_kernelMat+1] <= dataInSignal[(32 - 1) : 16];
-              // PLACE_HOLDER_KERNEL_MATRIX_2
               kernelMatrix[cnt_index_kernelMat+2] <= dataInSignal[(48 - 1) : 32];
-              // PLACE_HOLDER_KERNEL_MATRIX_3
               kernelMatrix[cnt_index_kernelMat+3] <= dataInSignal[(64 - 1) : 48];
-              // PLACE_HOLDER_KERNEL_MATRIX_4
               cnt_index_kernelMat <= cnt_index_kernelMat + 4;
               if (cnt_index_inMat >=  SIZE_INPUT_MAT ) begin
                 read_inMat <= 0;
